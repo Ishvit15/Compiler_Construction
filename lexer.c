bec86e092ae78048ef91aee9f7ad0e56236d78e7
Animesh Bhargava <adi.ani.funi@gmail.com>
2023-02-24 15:04:21 +0530

    create lexer.c

---
+++ lexer.c
@@ -0,0 +1,18 @@
+/* Functions to code:
+
+fillBuffer() {
+
+    makes use of following function:
+        size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
+}
+
+
+getChar()
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *fp;
